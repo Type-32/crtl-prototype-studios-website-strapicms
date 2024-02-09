c48fe75fff8f7097c58fcd954889d21f38ed86fe
Type-32 <wilsonsu2008@hotmail.com>
2024-02-09 10:04:46 +0800

    Added MinIO File Upload Endpoint

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,25 @@
-export default () => ({});
+import {region} from "@smithy/signature-v4/dist-types/suite.fixture";
+
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        s3Options:{
+          accessKeyId: env('MINIO_ACCESS_KEY_ID'),
+          secretAccessKey: env('MINIO_ACCESS_SECRET'),
+          endpoint: env('MINIO_ENDPOINT'),
+          region: 'us-west-rack2', // set as default on the minio config
+          params: {
+            Bucket: env('MINIO_BUCKET'),
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
